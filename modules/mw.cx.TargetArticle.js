e503f235899d84b897dd28338051925d8b67e535
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-29 22:55:58 +0300

    Update mw.cx.TargetArticle.js

--- modules/mw.cx.TargetArticle.js
+++ modules/mw.cx.TargetArticle.js
@@ -317,32 +317,32 @@ mw.cx.TargetArticle.prototype.publishFail = function (errorCode, messageOrFailOb
 	let mddxlink = "OAuth session expired, Please Log again to <a href='https://mdwiki.toolforge.org/Translation_Dashboard/auth.php?a=login' target='_blank'>Translation Dashboard</a>";
 	const editError = data.error;
 
-	// {"result":"error","edit":{"error":"noaccess","username":"Mr. Ibrahem"}}
-	if (data?.edit?.error) {
-		let noaccess_errors = [
-			"mwoauth-invalid-authorization",
-			"noaccess",
-			"no access",
-		];
-		if (noaccess_errors.includes(data.edit.error) || (data.edit.error && noaccess_errors.includes(data.edit.error.code))) {
-			this.showPublishError(mddx, "no access_keys in Translation_Dashboard");
-			// $('.cx-message-widget-message').html(mddxlink)
-			$('.cx-message-widget-message')
-				.empty()
-				.append(
-					$('<span>').text('OAuth session expired, Please Log again in '),
-					$('<a>')
-						.attr('href', 'https://mdwiki.toolforge.org/Translation_Dashboard/auth.php?a=login')
-						.attr('target', '_blank')
-						.text('Translation Dashboard')
-				);
-			$('.cx-message-widget-details').html(" Then refresh the page");
-			// $('.cx-message-widget-details').html("<a href='https://mdwiki.toolforge.org/Translation_Dashboard/auth.php?a=login' target='_blank'>Translation Dashboard</a>")
-			return;
-		}
-	}
-
 	if (editError) {
+		// {"result":"error","edit":{"error":"noaccess","username":"Mr. Ibrahem"}}
+		if (data?.edit?.error) {
+			let noaccess_errors = [
+				"mwoauth-invalid-authorization",
+				"noaccess",
+				"no access",
+			];
+			if (noaccess_errors.includes(data.edit.error) || (data.edit.error && noaccess_errors.includes(data.edit.error.code))) {
+				this.showPublishError(mddx, "no access_keys in Translation_Dashboard");
+				// $('.cx-message-widget-message').html(mddxlink)
+				$('.cx-message-widget-message')
+					.empty()
+					.append(
+						$('<span>').text('OAuth session expired, Please Log again in '),
+						$('<a>')
+							.attr('href', 'https://mdwiki.toolforge.org/Translation_Dashboard/auth.php?a=login')
+							.attr('target', '_blank')
+							.text('Translation Dashboard')
+					);
+				$('.cx-message-widget-details').html(" Then refresh the page");
+				// $('.cx-message-widget-details').html("<a href='https://mdwiki.toolforge.org/Translation_Dashboard/auth.php?a=login' target='_blank'>Translation Dashboard</a>")
+				return;
+			}
+		}
+
 		// Handle spam blacklist error (either from core or from Extension:SpamBlacklist)
 		// Example of API result - https://phabricator.wikimedia.org/P8991
 		if (editError.code === 'spamblacklist') {
